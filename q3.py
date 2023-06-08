72bd7f44e217d93a80cc85d1ff1a4157608b7334
Dhanukshan <130682373+Dhanukshan@users.noreply.github.com>
2023-06-08 09:32:39 +0530

    Add files via upload

--- q3.py
+++ q3.py
@@ -1,11 +1,7 @@
-a=int(input("enter the starting number:"))
-b=int(input("enter the ending number:"))
-print("The prime numbers in the range are:")
-for i in range(a,b+1):
-    if i>1:
-        for j in range(2,i):
-            if(i%j==0):
-                break
-        else:
-            print(i)
-            
+speed=int(input("enter wind speed in kilometers per hour="))
+temp=int(input("enter temperature in degree celsius="))
+t=temp
+v=speed
+wci=13.12+0.6215*t-11.37*v**0.16+0.3965*t*v**0.16
+windchillindex=round(wci)
+print("wind chill index=",windchillindex)
